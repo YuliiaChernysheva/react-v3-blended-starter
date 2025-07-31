8bec92969edc2a5c3f5b8256dd27d4485f69588a
Yuliia <chernyshevaulia2905@gmail.com>
2025-07-31 14:17:08 -0600

    added everything

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,87 @@
 import Modal from "../Modal/Modal";
-import PostList from "../PostList/PostList";
+import { useState } from "react";
 import SearchBox from "../SearchBox/SearchBox";
-import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { fetchPosts } from "../../services/postService";
+import { useDebounce } from "use-debounce";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import PostList from "../PostList/PostList";
+import Pagination from "../Pagination/Pagination";
+import CreatePostForm from "../CreatePostForm/CreatePostForm";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isCreatePost, setIsCreatePost] = useState<boolean>(false);
+
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
+
+  const { data } = useQuery({
+    queryKey: ["posts", debouncedSearchQuery, currentPage],
+    queryFn: () => fetchPosts(debouncedSearchQuery, currentPage),
+    placeholderData: keepPreviousData,
+  });
+
+  const changeSearchQuery = (newQuery: string) => {
+    setCurrentPage(1);
+    setSearchQuery(newQuery);
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const toggleCreatePost = () => {
+    setIsCreatePost(!isCreatePost);
+  };
+
+  const posts = data?.posts ?? [];
+  const totalPages = data?.totalCount ? Math.ceil(data.totalCount / 8) : 0;
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox />
-        <Pagination />
-        <button className={css.button}>Create post</button>
+        <SearchBox value={searchQuery} onSearch={changeSearchQuery} />
+        {totalPages > 1 && (
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        )}
+
+        <button
+          className={css.button}
+          onClick={() => {
+            toggleModal();
+            toggleCreatePost();
+          }}
+        >
+          Create post
+        </button>
       </header>
-      <Modal>{/* Передати через children компонент CreatePostForm або EditPostForm */}</Modal>
-      <PostList />
+
+      {isModalOpen && (
+        <Modal
+          onClose={() => {
+            toggleModal();
+            toggleCreatePost();
+          }}
+        >
+          {isCreatePost && (
+            <CreatePostForm
+              onClose={() => {
+                toggleModal();
+                toggleCreatePost();
+              }}
+            />
+          )}
+        </Modal>
+      )}
+
+      {posts.length > 0 && <PostList posts={posts} />}
     </div>
   );
 }
